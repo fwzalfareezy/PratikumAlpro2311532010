d7f13706870e64deaad60d83b8f2618ff5ad53b6
fwzalfareezy <146146871+fwzalfareezy@users.noreply.github.com>
2023-11-21 21:41:36 +0700

    PEKAN5

---
+++ 2311532010_MuhammadAlfarizy/src/pekan5/While3.java
@@ -0,0 +1,38 @@
+package pekan5;
+
+import java.util.Scanner;
+
+public class While3 {
+
+	public static void main(String[] args) {
+		// deklarasi variabel
+		int umur;
+		char sim;
+		String jawab="ya";
+		// deklarasi scanner
+		Scanner scan = new Scanner(System.in);
+		while (!jawab.equalsIgnoreCase("tidak")) {
+			System.out.println("Input Umur Anda : ");
+			umur = scan.nextInt();
+			System.out.println("Apakah Anda Sudah Punya Sim? (Y/N) : ");
+			sim = scan.next().charAt(0);	
+			if((umur >= 17) &&(sim == 'Y')) {
+				System.out.println("Anda Sudah Dewasa Dan Boleh membawa motor");
+			}
+			if((umur >= 17) &&(sim != 'Y')) {
+				System.out.println("Anda Sudah Dewasa namun Boleh membawa motor");
+			}
+			if((umur < 17) &&(sim == 'Y')) {
+				System.out.println("Anda Belum Dewasa namun Boleh membawa motor");
+			}
+			if((umur < 17) &&(sim != 'Y')) {
+				System.out.println("Anda Belum Dewasa dan belum boleh membawa motor");
+			}
+			System.out.println("Apakah lanjut (ya / tidak?)");
+			jawab= scan.next();
+			}
+			System.out.println("Terimakasih sudah menggunakan program kami");
+			scan.close();
+	}
+}
+
